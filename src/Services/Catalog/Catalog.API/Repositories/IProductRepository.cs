34e8fed6a4e53b5e9bb0b4839f1b5b0bdc1530d8
rquadrelli <50831707+rquadrelli@users.noreply.github.com>
2022-05-03 15:32:05 +0200

    Commit 03/05/2022 15:31

--- src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -10,7 +10,6 @@ namespace Catalog.API.Repositories
         Task<Product> GetProduct(string id);
         Task<IEnumerable<Product>> GetProductByName(string name);
         Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
-
         Task CreateProduct(Product product);
         Task<bool> UpdateProduct(Product product);
         Task<bool> DeleteProduct(string id);
